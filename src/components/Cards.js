1fd4c8f413613629c924ca730824e89cd027c83f
Luke <Luke@Lukes-MacBook-Pro.local>
2020-11-12 12:29:08 +0000

    cards
    edited CardItem.js and added a Cards.js component

---
+++ src/components/Cards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cards() {
+    return (
+        <div className='cards'>
+            <h1>Check out our epic cities!</h1>
+        </div>
+    )
+}
+
+export default Cards
