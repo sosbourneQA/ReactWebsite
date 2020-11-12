5bd85da078c2c1b1b1d784c55933f8bab8265229
Luke <Luke@Lukes-MacBook-Pro.local>
2020-11-12 15:13:39 +0000

    Update CardItem.js
    added some props code

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -2,18 +2,18 @@
  import { Link } from 'react-router-dom';
 
  
- function CardItem() {
+ function CardItem(props) {
      return (
          <>
              <li className="cards__item" >
              <Link 
              className="cards__item__link">
-                 <figure className="cards__item__pic-wrap">
-                    <img src="" alt="Travel Image" className="cards__item__img">
+                 <figure className="cards__item__pic-wrap" dataCategory={props.label}>
+                    <img src={props.src} alt="Travel Image" className="cards__item__img" >
                     </img>
                  </figure>
                  <div className="cards__item__info">
-                    <h5 className="cards__item_text"/>
+                    <h5 className="cards__item_text">{props.text}</h5>
                  </div>
              </Link>
              </li>
