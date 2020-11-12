af3b2d7c1e060eecdb6bd6f38e50ee717abdd269
Luke <Luke@Lukes-MacBook-Pro.local>
2020-11-12 15:01:15 +0000

    cards
    added CardItem component to Cards.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
 import HeroElement from '../HeroElement';
+import Cards from '../Cards';
 
 function Home () {
     return (
         <>
             <HeroElement/> 
+            <Cards/>
         </>
     )
 }
