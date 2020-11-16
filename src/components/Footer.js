701d9c980e84926823af076ca67fd16189f781c0
Luke <Luke@Lukes-MacBook-Pro.local>
2020-11-16 18:13:20 +0000

    footer
    completed the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,99 +5,114 @@ import {Link} from 'react-router-dom'
 
 function Footer() {
     return (
-        <div className='footer-container'>
-            <section className='footer-subscription'>
-                <p>
-                    Join the adventure today!
-                </p>
-                <div className='input-areas'>
-                    <form>
-                        <input
-                            type='email'
-                            name='email'
-                            placeholder='Your Email'
-                            className='footer-input'
-                        />
-                        <Button buttonStyle='btn--outline'>Join Us</Button>
-                    </form>
-                </div>
-            </section>
-            <div className='footer-links'>
-                <div className='footer-link-wrapper'>
-                    <div className='footer-link-items'>
-                        <h2>How it works</h2>
-                        <Link to='/sign-up'>SIGN UP</Link>
-                        <Link to='/'>CAREERS</Link>
-                        <Link to='/'>TERMS OF SERVICE</Link>
-                    </div>
-                    <div className='footer-link-items'>
-                        <h2>Contact us</h2>
-                        <Link to='/sign-up'>SIGN UP</Link>
-                        <Link to='/'>CAREERS</Link>
-                        <Link to='/'>TERMS OF SERVICE</Link>
-                    </div>
-                    <div className='footer-link-items'>
-                        <h2>Social media</h2>
-                        <Link to='/sign-up'>SIGN UP</Link>
-                        <Link to='/'>CAREERS</Link>
-                        <Link to='/'>TERMS OF SERVICE</Link>
-                    </div>
-                    <div className='footer-link-items'>
-                        <h2>Videos</h2>
-                        <Link to='/sign-up'>SIGN UP</Link>
-                        <Link to='/'>CAREERS</Link>
-                        <Link to='/'>TERMS OF SERVICE</Link>
-                    </div>
-                </div>
+      <div className='footer-container'>
+        <section className='footer-subscription'>
+          <p className='footer-subscription-heading'>
+            Join the adventure today
+          </p>
+          
+          <div className='input-areas'>
+            <form>
+              <input
+                className='footer-input'
+                name='email'
+                type='email'
+                placeholder='Your Email'
+              />
+              <Button buttonStyle='btn--outline'>Join us</Button>
+            </form>
+          </div>
+        </section>
+        <div class='footer-links'>
+          <div className='footer-link-wrapper'>
+            <div class='footer-link-items'>
+              <h2>About Us</h2>
+              <Link to='/sign-up'>How it works</Link>
+              <Link to='/'>Testimonials</Link>
+              <Link to='/'>Careers</Link>
+              <Link to='/'>Investors</Link>
+              <Link to='/'>Terms of Service</Link>
             </div>
-            <section>
-                <div>
-                    <div className='footer-logo'>
-                        <Link
-                        className='social-icon-link facebook' 
-                        to='/'
-                        targets='_blank'
-                        aria-label='Facebook'
-                        >
-                            <i className='fab fa-facebook-f'/>
-                        </Link >
-                        <Link
-                        className='social-icon-link instagram' 
-                        to='/'
-                        targets='_blank'
-                        aria-label='Instagram'
-                        >
-                            <i className='fab fa-instagram'/>
-                        </Link >
-                        <Link
-                        className='social-icon-link twitter' 
-                        to='/'
-                        targets='_blank'
-                        aria-label='Twitter'
-                        >
-                            <i className='fab fa-twitter'/>
-                        </Link >
-                        <Link
-                        className='social-icon-link youtube' 
-                        to='/'
-                        targets='_blank'
-                        aria-label='Youtube'
-                        >
-                            <i className='fab fa-youtube'/>
-                        </Link >
-                        <Link
-                        className='social-icon-link linkedin' 
-                        to='/'
-                        targets='_blank'
-                        aria-label='LinkedIn'
-                        >
-                            <i className='fab fa-linkedin'/>
-                        </Link >
-                    </div>
-                </div>
-            </section>
+            <div class='footer-link-items'>
+              <h2>Contact Us</h2>
+              <Link to='/'>Contact</Link>
+              <Link to='/'>Support</Link>
+              <Link to='/'>Destinations</Link>
+              <Link to='/'>Sponsorships</Link>
+            </div>
+          </div>
+          <div className='footer-link-wrapper'>
+            <div class='footer-link-items'>
+              <h2>Videos</h2>
+              <Link to='/'>Submit Video</Link>
+              <Link to='/'>Ambassadors</Link>
+              <Link to='/'>Agency</Link>
+              <Link to='/'>Influencer</Link>
+            </div>
+            <div class='footer-link-items'>
+              <h2>Social Media</h2>
+              <Link to='/'>Instagram</Link>
+              <Link to='/'>Facebook</Link>
+              <Link to='/'>Youtube</Link>
+              <Link to='/'>Twitter</Link>
+            </div>
+          </div>
         </div>
-    )
-}
+        <section class='social-media'>
+          <div class='social-media-wrap'>
+            <div class='footer-logo'>
+              <Link to='/' className='social-logo'>
+                Urbs
+                <i class='fab fa-typo3' />
+              </Link>
+            </div>
+            <small class='website-rights'>Urbs © 2020</small>
+            <div class='social-icons'>
+              <Link
+                class='social-icon-link facebook'
+                to='/'
+                target='_blank'
+                aria-label='Facebook'
+              >
+                <i class='fab fa-facebook-f' />
+              </Link>
+              <Link
+                class='social-icon-link instagram'
+                to='/'
+                target='_blank'
+                aria-label='Instagram'
+              >
+                <i class='fab fa-instagram' />
+              </Link>
+              <Link
+                class='social-icon-link youtube'
+                to='/'
+                target='_blank'
+                aria-label='Youtube'
+              >
+                <i class='fab fa-youtube' />
+              </Link>
+              <Link
+                class='social-icon-link twitter'
+                to='/'
+                target='_blank'
+                aria-label='Twitter'
+              >
+                <i class='fab fa-twitter' />
+              </Link>
+              <Link
+                class='social-icon-link twitter'
+                to='/'
+                target='_blank'
+                aria-label='LinkedIn'
+              >
+                <i class='fab fa-linkedin' />
+              </Link>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 
-export default Footer
+export default Footer;
